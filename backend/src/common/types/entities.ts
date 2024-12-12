d32feab75dd08edefd918ad579a9aca32d8d189c
Elazar Avrahami <elazar252@gmail.com>
2024-12-13 01:48:57 +0200

    Just test

--- backend/src/common/types/entities.ts
+++ backend/src/common/types/entities.ts
@@ -1,4 +1,3 @@
-import { Account } from "../../db/db-entities/account.entity";
 import { User } from "../../db/db-entities/user.entity";
 
-export type Entity = User | Account;
+export type Entity = User;
