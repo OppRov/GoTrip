89a86280ee6a720f87f955ccdd23f6060f5f009d
Elazar Avrahami <elazar252@gmail.com>
2024-12-26 13:55:02 +0200

    Fix changes in the frontend

--- frontend/src/components/CalendarDisplay.jsx
+++ frontend/src/components/CalendarDisplay.jsx
@@ -3,31 +3,6 @@ import * as React from "react";
 import FullCalendar from "@fullcalendar/react";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import dayGridPlugin from "@fullcalendar/daygrid";
-import { useState } from "react";
-
-export default function CalendarDisplay() {
-  const [visibleRange, setVisibleRange] = useState({
-    start: new Date(),
-    end: new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000),
-  });
-
-  return (
-    <FullCalendar
-      plugins={[timeGridPlugin, dayGridPlugin]}
-      initialView="timeGridWeek"
-      height={"100%"}
-      aspectRatio={0.5}
-      visibleRange={visibleRange}
-      slotDuration={"00:60:00"}
-      events={[
-        { title: "event 1", date: "2024-12-01" },
-        { title: "event 2", date: "2024-12-02" },
-      ]}
-      views={{
-        timeGridWeek: {
-          type: "timeGrid",
-          duration: { days: 3 },
-        },
 import timelimePlugin from "@fullcalendar/timeline";
 import { useState } from "react";
 import { useRef, useEffect } from "react";
--- frontend/src/components/planner/ResultView.jsx
+++ frontend/src/components/planner/ResultView.jsx
@@ -1,7 +1,3 @@
-import React from "react";
-
-const ResultView = () => {
-  return <div>ResultView</div>;
 import React, { useContext } from "react";
 import CalendarDisplay from "../CalendarDisplay";
 import { planContext } from "../../contexts/planContext";
